c427c662bae1282fd95a3969d956672faba4e411
Deokwoo Jung <deokwooj@gmail.com>
2015-07-26 17:49:40 +0900

    Create na_analysis.py

---
+++ na_analysis.py
@@ -0,0 +1,15 @@
+# this is news source analyis program. 
+# read articles and discover network structure of news sources based on quataions in artticles. 
+
+# defintion of class strutures. 
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
