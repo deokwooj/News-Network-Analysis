02f6b0314f1fdea89f5fbf3e4699d22253757f55
Deokwoo Jung <deokwooj@gmail.com>
2015-08-23 22:11:01 +0900

    Update na_analysis.py

--- na_analysis.py
+++ na_analysis.py
@@ -3,7 +3,7 @@
 # this is news source analyis program. 
 # read articles and discover network structure of news sources based on quataions in artticles. 
 
-# Deokwoo Jung 's update 23 Aug. 
+# Deokwoo Jung 's update 23 Aug by jdw. 
 # modules to be imported
 from __future__ import division # To forace float point division
 import os
